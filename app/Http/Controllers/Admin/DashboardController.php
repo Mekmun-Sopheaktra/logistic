eda14bdcf4c9035199ee160676590e805632c79c
Mekmun Sopheaktra <Mekmunsopheaktra@gmail.com>
2025-03-02 23:07:43 +0700

    Update

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -37,7 +37,7 @@ class DashboardController extends Controller
                 'vendor_name' => Vendor::query()->where('user_id', $vendor->id)->first()->first_name . ' ' . Vendor::query()->where('user_id', $vendor->id)->first()->last_name,
                 'vendor_address' => Vendor::query()->where('user_id', $vendor->id)->first()->address,
                 'total_delivery' => Package::query()->where('vendor_id', $vendor->id)->where('status', 'completed')->count(),
-                $amount = Package::query()
+                'amount' => $amount = Package::query()
                     ->where('vendor_id', $vendor->id)
                     ->where('status', 'completed')
                     ->with('shipment')
